cf78a15ca2ebd7532098be0d0764099e5b951d4c
RefiDaus <92767593+RefiDaus@users.noreply.github.com>
2023-03-14 10:45:50 +0700

    Add files via upload

---
+++ Codingan/kondisi switch.php
@@ -0,0 +1,21 @@
+<?php
+$hari = "Senin";
+
+if ($hari == "Senin") {
+    echo "Hari ini adalah hari Senin.";
+} else if ($hari == "Selasa") {
+    echo "Hari ini adalah hari Selasa.";
+} else if ($hari == "Rabu") {
+    echo "Hari ini adalah hari Rabu.";
+} else if ($hari == "Kamis") {
+    echo "Hari ini adalah hari Kamis.";
+} else if ($hari == "Jumat") {
+    echo "Hari ini adalah hari Jumat.";
+} else if ($hari == "Sabtu") {
+    echo "Hari ini adalah hari Sabtu.";
+} else if ($hari == "Minggu") {
+    echo "Hari ini adalah hari Minggu.";
+} else {
+    echo "Input tidak valid.";
+}
+?>
